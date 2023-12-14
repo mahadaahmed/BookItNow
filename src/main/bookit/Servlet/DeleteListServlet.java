3ae0e930650661deca73111d346f9cf241be4d54
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-12-14 20:22:48 +0100

    deleting a list servlet

---
+++ src/main/bookit/Servlet/DeleteListServlet.java
@@ -0,0 +1,36 @@
+package main.bookit.Servlet;
+
+import main.bookit.DAO.ListDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// In DeleteListServlet.java
+@WebServlet("/deleteList")
+public class DeleteListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Similar admin check as above
+        // Get list ID from request
+        int listId = Integer.parseInt(request.getParameter("listId"));
+
+        // Use ListDAO to delete the list
+        ListDAO listDao = new ListDAO();
+        boolean isSuccess = listDao.deleteList(listId);
+
+        // Handle redirect or forward based on success or failure
+
+        if (isSuccess) {
+            // Redirect or forward to success page or admin dashboard
+            //request.getRequestDispatcher("dashboard.jsp").forward(request, response);
+            response.sendRedirect("dashboard.jsp");
+        } else {
+            // Handle failure: set error message and forward back to form
+            request.setAttribute("errorMessage", "Something went wrong when creating the list");
+        }
+    }
+}
